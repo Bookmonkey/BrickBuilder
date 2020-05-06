5ac39b2c6d91b6e15e0f24d0909a89a244631b5c
Daniel Dickson <1097211+Bookmonkey@users.noreply.github.com>
2020-05-06 19:19:55 +1200

    UI works with Vue fully. Add BrickController as 'engine'

--- client/UIView.vue
+++ client/UIView.vue
@@ -1,30 +1,39 @@
 <template>
     <div class="ui">
-      <div class="selected-brick">
-        <div id="brick-name">as</div>
-      </div>
+        <div class="settings">
+          <img src="images/settings.svg"/>
+        </div>
+      <div class="brick-picker">
+        <div class="selected-brick">
+          <div id="brick-name">Bricks
+          </div>
+        </div>
 
-      <div class="bricks">
-        <div class="ui-add" @click="addBrick()" id="ui-add" data-x="1" data-y="1" data-h="10.0">Add 1x1</div>
-        <div class="ui-add" id="ui-add" data-x="1" data-y="4" data-h="10.0">Add 1x4</div>
-      </div>
+        <div class="bricks">
+          <div class="ui-add" v-for="item in bricks" @click="addBrick(item)">
+            {{ item.title }}
+          </div>
+        </div>
 
-      <div class="config">
-        <select name="ui-colour" id="ui-colour">
-          <option value="red">Red</option>
-          <option value="green">Green</option>
-          <option value="blue">Blue</option>
-          <option value="yellow">Yellow</option>
-          <option value="black">Black</option>
-          <option value="purple">Purple</option>
-          <option value="white">White</option>
-        </select>
-      </div>
+        <div class="config">
+          <div class="select" @click="colourDropdown = !colourDropdown">
+            <div class="selected-item">
+              <span class="colour" :class="brickColour.class"></span> {{ brickColour.name }}
+            </div>
 
-      <div class="misc">
-        <div class="ui-chat-tab"></div>
-        <div class="ui-status">Connected</div>
-        <div class="ui-head-count">1 person</div>
+            <div class="items" v-if="colourDropdown">
+              <div class="item" v-for="colour in brickColours" @click="setColour(colour)">
+                <span class="colour" :class="colour.class"></span> {{ colour.name }}
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="misc">
+          <div class="ui-chat-tab"></div>
+          <div class="ui-status">Connected</div>
+          <div class="ui-head-count">1 person</div>
+        </div>
       </div>
     </div>
 </template>
@@ -33,21 +42,90 @@
 import Vue from "vue";
 
 import BrickController from "./BrickController";
+import { BrickColours, BrickList } from "./utils/config";
 
-const bricks = new BrickController();
+
+const brickController = new BrickController();
 export default Vue.extend({
   data() {
     return {
-      bundler: "Parcel"
-    };
+      brickColours: BrickColours,
+      brickColour: BrickColours[0],
+      colourDropdown: false,
+      bricks: BrickList,
+    }    
   },
   methods: {
-    addBrick() {
-        bricks.addBrick();    
+    addBrick(brickId) {      
+        brickController.addBrick(brickId);
+    },
+    setColour(colour) {
+      this.brickColour = colour;
+
+      brickController.colour = colour.class;
     }
-  }
+  },
 });
 </script>
 
 <style lang="scss" scoped>
+.button,
+.select > .selected-item {
+  border-radius: 6px;
+  box-shadow: inset 0 0 0 1px #d1d5da;
+  padding: 12px 24px;
+  font-size: 16px;
+
+  display: inline-block;
+  cursor: pointer;
+}
+
+.select::hover, .button::hover {
+  background: #fff;
+}
+
+
+.select > .items {
+  display: block;
+  position: absolute;
+  bottom: 60px;
+  width: 130px;
+  box-shadow: inset 0 0 0 1px #d1d5da;
+  background: white;
+}
+
+.select .items .item {
+  padding: 6px;
+}
+
+.select .items .item:hover {
+  background: #d1d5da;
+}
+
+
+.colour {
+  border-radius: 50%;
+  margin-right: 4px;
+  display: inline-block;
+  width: 12px;
+  height: 12px;
+}
+
+.colour.red {
+  background: red;
+}
+
+.colour.blue {
+  background: blue;
+}
+.colour.green {
+  background: green;
+}
+.colour.yellow {
+  background: yellow;
+}
+.colour.black {
+  background: black;
+}
+
 </style>
