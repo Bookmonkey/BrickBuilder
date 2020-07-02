95c56a5db0e764353c31d8fed6d7a032cffb6bc2
Daniel Dickson <1097211+Bookmonkey@users.noreply.github.com>
2020-07-02 15:33:46 +1200

    Add ColourPicker rename scss to match component

---
+++ client/components/ColourPicker.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="colour-picker">
+    <div class="options">
+
+    </div>
+
+    <button class="button sm">Colours</button>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ColourPicker",
+  props: [
+    "colour",
+  ],
+  data(){
+    return {}
+  }
+}
+</script>
+
+<style>
+
+.options {
+  width: 300px;
+  height: 300px;
+}
+
+</style>
